94d6475b5c385b6433e72fe71483ad5ce3af07a4
valeriaadelgado <valerydelgadh@gmail.com>
2024-10-19 17:21:04 -0600

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,7 +1,18 @@
 '''
-
+Un vendedor recibe un sueldo base mas de 10% extra por comision de sus ventas, el vendedor desea 
+saber cuanto  dinero obtendra por concepto de comisiones por las tres ventas que realiza en el mes y
+el total que recibira en el mes tomando en cuenta su sueldo base y comisiones
 '''
-total = float(input("Cual fue el total de la compra: "))
-d = (total)*0.15
-td = (total-d)
-print("El total con el descuento es:",td)
+v1 = input("Ingresa la venta 1: ")
+v1 = int(v1)
+v2 = input("ingresa la venta 2: ")
+v2 = int(v2)
+v3 = input("ingresa la venta 3: ")
+v3 = int(v3)
+c1 = (v1*0.10)
+c2 = (v2*0.10)
+c3 = (v3*0.10)
+ct = (c1+c2+c3)
+vt = (v1+v2+v3)
+sueldo = (ct + vt)
+print ("El sueldo es de:",sueldo)
