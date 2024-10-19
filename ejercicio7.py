2fa7a95c78b549eb326517dba572f52dd8855755
Valeria <valerydelgad@gmail.com>
2024-10-19 16:57:56 -0600

    ejercicio 7

---
+++ ejercicio7.py
@@ -0,0 +1,16 @@
+'''
+
+'''
+v1 = input("Ingresa la venta 1: ")
+v1 = int(v1)
+v2 = input("ingresa la venta 2: ")
+v2 = int(v2)
+v3 = input("ingresa la venta 3: ")
+v3 = int(v3)
+c1 = (v1*0.10)
+c2 = (v2*0.10)
+c3 = (v3*0.10)
+ct = (c1+c2+c3)
+vt = (v1+v2+v3)
+sueldo = (ct + vt)
+print ("El sueldo es de:",sueldo)
---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+'''
+
+'''
+total = float(input("Cual fue el total de la compra: "))
+d = (total)*0.15
+td = (total-d)
+print("El total con el descuento es:",td)
